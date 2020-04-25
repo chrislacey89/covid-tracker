f1e6124194eb7cb4648ed1b762aa61b3d8e76967
Chris Lacey <ChrisLacey89@gmail.com>
2020-04-25 09:08:48 -0400

    set up async fetching of data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Cards from './Components/Cards/Cards';
+import Chart from './Components/Chart/Chart';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+
+import styles from './App.module.css';
+
+import { fetchData } from './api';
 
 function App() {
+	useEffect(() => {
+		// Create an scoped async function in the hook
+		async function anyNameFunction() {
+			const data = await fetchData();
+			console.log(data);
+		}
+		// Execute the created function directly
+		anyNameFunction();
+	}, []);
+
 	return (
-		<div>
-			<h1>hello world</h1>
+		<div className={styles.container}>
+			<Cards></Cards>
+			<CountryPicker></CountryPicker>
+			<Chart></Chart>
 		</div>
 	);
 }
