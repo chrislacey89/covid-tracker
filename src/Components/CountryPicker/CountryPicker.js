a9fb7502fba129ff3f6dc2d9c0d17c05fd564f91
Chris Lacey <ChrisLacey89@gmail.com>
2020-05-02 09:05:47 -0400

    countryPicker Autocomplete setup

--- src/Components/CountryPicker/CountryPicker.js
+++ src/Components/CountryPicker/CountryPicker.js
@@ -1,29 +1,55 @@
-import React, {useEffect, useState }from 'react';
-import { Select, FormControl, MenuItem } from '@material-ui/core';
+/* eslint-disable no-use-before-define */
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import parse from "autosuggest-highlight/parse";
+import match from "autosuggest-highlight/match";
 
-import { fetchCountries } from '../../api'
-
-
+import { fetchCountries } from "../../api";
 
 const CountryPicker = () => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const countryNames = await fetchCountries();
+      setFetchedCountries(countryNames);
+    };
+    fetchAPI();
+    console.log(fetchedCountries);
+  }, []);
 
-const [fetchedCountries, setFetchedCountries ]= useState([]) 
+  return (
+    <Autocomplete
+      id="highlights-demo"
+      style={{ width: 300 }}
+      options={fetchedCountries}
+      getOptionLabel={(option) => option.name}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Highlights"
+          variant="outlined"
+          margin="normal"
+        />
+      )}
+      renderOption={(option, { inputValue }) => {
+        const matches = match(option.name, inputValue);
+        const parts = parse(option.name, matches);
 
-	useEffect(()=> {
-		const fetchAPI = async () => {
-		const countryNames = await fetchCountries()
-		setFetchedCountries(countryNames)
-		}
-		fetchAPI()
-		console.log(fetchedCountries)
-	}, [setFetchedCountries])
-	return (
-		<FormControl>
-			<Select>
-				<MenuItem value={10}>Ten</MenuItem>
-			</Select>
-		</FormControl>
-	)
+        return (
+          <div>
+            {parts.map((part, index) => (
+              <span
+                key={index}
+                style={{ fontWeight: part.highlight ? 700 : 400 }}
+              >
+                {part.text}
+              </span>
+            ))}
+          </div>
+        );
+      }}
+    />
+  );
 };
-
 export default CountryPicker;
--- src/api/index.js
+++ src/api/index.js
@@ -1,43 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api';
+const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
-	try {
-		const  {data}  = await axios.get(url);
+  try {
+    const { data } = await axios.get(url);
 
-		const modifiedData = {
-			confirmed: data.confirmed,
-			recovered: data.recovered,
-			deaths: data.deaths,
-			lastUpdate: data.lastUpdate
-		}
+    const modifiedData = {
+      confirmed: data.confirmed,
+      recovered: data.recovered,
+      deaths: data.deaths,
+      lastUpdate: data.lastUpdate,
+    };
 
-		return modifiedData;
-	} catch (error) {
-		return error;
-	}
+    return modifiedData;
+  } catch (error) {
+    return error;
+  }
 };
 
 export const fetchDailyData = async () => {
-	try {
-		const { data } = await axios.get(`${url}/daily`);
-		return data
-
-	} catch (error) {
-		return error;
-	}
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 export const fetchCountries = async () => {
-	try {
-		const res = await axios.get(`${url}/countries`);
-		const countries = res.data.countries.map(country => country.name)
-			return countries
-	} catch (error) {
-		return error;
-	}
+  try {
+    const res = await axios.get(`${url}/countries`);
+    const countries = res.data.countries;
+    return countries;
+  } catch (error) {
+    return error;
+  }
 };
-
-
-
